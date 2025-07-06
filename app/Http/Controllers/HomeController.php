b1a47f446a10333f84d67af83619540688281cdc
Zacky Achmad <hi@zacky.id>
2025-07-06 09:12:19 +0700

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,6 @@ class HomeController extends Controller
         }
 
         return view('home', [
-            'title'      => 'Home',
             'tweetUrl'   => $tweetUrl,
             'autoSearch' => $autoSearch,
         ]);
